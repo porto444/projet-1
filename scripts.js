1cc7ec0d20ada7bc9872645fa2318916b0da55d9
porto444 <adbelrhazi@gmail.com>
2024-03-21 23:31:29 +0100

    new code

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function loadRandomCategory() {
-    var categories = ['L', 'D', 'Sushi', 'Drinks', 'Desserts'];
-    var randomIndex = Math.floor(Math.random() * categories.length);
-    var randomCategoryShortName = categories[randomIndex];
-    $dc.loadMenuItems(randomCategoryShortName);
-}
